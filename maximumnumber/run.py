b726e3e76bf232da95ab93b43925448bf6466dcd
AliMahmudov <mahmudoveli2007@gmail.com>
2024-03-21 17:18:17 +0400

    task is complete

--- maximumnumber/run.py
+++ maximumnumber/run.py
@@ -1,1 +1,14 @@
-print('hello world')
+
+num=int(input("enter the number: "))
+t=int(input("enter the number of operations: "))
+
+def findTheAchiaveableMax(num,t):
+    if t<num:
+        min_value=t
+    max_increment=min_value*2
+    max_achievable_number=num+max_increment
+    print(max_achievable_number)
+
+findTheAchiaveableMax(num,t)
+
+ 
